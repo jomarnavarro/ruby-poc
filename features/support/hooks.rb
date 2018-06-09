819e620864438b2da7f39101e8799b91cd268169
Omar Navarro <jomarnavarro@gmail.com>
2018-06-09 17:58:06 -0500

    Fixes chrome options deprecated method

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,14 +1,14 @@
 def ensure_page(page_object_class)
 	return if @page.is_a?(page_object_class)
 	@page = page_object_class.new(@driver)
+	assert @page.is_at?
 end
 
 Before do |scenario|
 	@config = YAML.load_file('./features/support/props.conf')
-	@prefs = {credentials_enable_service: false}
-	caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"args" => [ "--disable-notifications" ]})
-
-	@driver = Selenium::WebDriver.for @config['browser'], prefs: @prefs, desired_capabilities: caps
+	options = Selenium::WebDriver::Chrome::Options.new
+	options.add_argument('--disable-notifications')
+	@driver = Selenium::WebDriver.for @config['browser'], options: options
 	@driver.navigate.to @config['url']
 
 	ensure_page(FacebookLoginPage)	
