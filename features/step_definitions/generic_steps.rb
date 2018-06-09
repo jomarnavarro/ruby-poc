9172fe1b5da4caf7c9abebf26c1d2e8f85d6cff7
Omar Navarro <jomarnavarro@gmail.com>
2018-06-09 17:59:04 -0500

    Moves at? method as an assertion in hooks.rb

--- features/step_definitions/generic_steps.rb
+++ features/step_definitions/generic_steps.rb
@@ -2,13 +2,10 @@ Given(/^User is on (.*)$/) do |page|
   case page
   when 'Facebook Login Page'
   		ensure_page FacebookLoginPage
-      @page.is_at?()
   when 'Facebook Home Page'
   		ensure_page FacebookHomePage
-      @page.is_at?()
   when 'Facebook Search Results Page'
   		ensure_page FacebookSearchResultsPage
-      @page.is_at?()
   end
 end
 
